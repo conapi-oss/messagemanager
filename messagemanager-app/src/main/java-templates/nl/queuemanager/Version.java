d14365c2c9fd4e16033b0cceeef5019a09bec03a
Gerco Dries <gerco.dries@adaptris.com>
2016-06-23 19:31:40 -0500

    Create a buildNumber from the time of building

--- messagemanager-app/src/main/java-templates/nl/queuemanager/Version.java
+++ messagemanager-app/src/main/java-templates/nl/queuemanager/Version.java
@@ -3,4 +3,5 @@ package nl.queuemanager;
 public class Version {
 	public static final String VERSION = "${project.version}";
 	public static final String SUFFIX = "${suffix}";
+	public static final String BUILD_ID = "${buildNumber}";
 }
