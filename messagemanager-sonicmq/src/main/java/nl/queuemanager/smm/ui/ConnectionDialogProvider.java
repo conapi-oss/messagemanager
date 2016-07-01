cf1e65c1959b5e4c3c212aa2358ca903bad42b08
Gerco Dries <gerco.dries@adaptris.com>
2016-06-30 20:40:56 -0500

    Remove standalone support for sonicmq

--- messagemanager-sonicmq/src/main/java/nl/queuemanager/smm/ui/ConnectionDialogProvider.java
+++ messagemanager-sonicmq/src/main/java/nl/queuemanager/smm/ui/ConnectionDialogProvider.java
@@ -1,11 +1,10 @@
 package nl.queuemanager.smm.ui;
 
-import nl.queuemanager.ui.util.DesktopHelper;
-
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 import com.google.inject.Provider;
 import com.sonicsw.ma.gui.domain.JDomainConnectionDialog;
+
+import nl.queuemanager.ui.util.DesktopHelper;
 
 /**
  * This provider solves the circular dependency that exists when we attempt to inject a
@@ -15,17 +14,15 @@ import com.sonicsw.ma.gui.domain.JDomainConnectionDialog;
  * the circular dependency.
  */
 public class ConnectionDialogProvider implements Provider<JDomainConnectionDialog> {
-	private final Injector injector;
 	private final DesktopHelper desktop;
 	
 	@Inject
-	public ConnectionDialogProvider(Injector injector, DesktopHelper desktop) {
-		this.injector = injector;
+	public ConnectionDialogProvider(DesktopHelper desktop) {
 		this.desktop = desktop;
 	}
 
 	public JDomainConnectionDialog get() {
-		JDomainConnectionDialog dialog = new JDomainConnectionDialog(injector.getInstance(SMMFrame.class));
+		JDomainConnectionDialog dialog = new JDomainConnectionDialog(null);
 		desktop.makeMacSheet(dialog);
 		return dialog;
 	}
