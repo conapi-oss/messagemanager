fcd00e1d9ad1d8d2271a287007438b29dc9c1ed4
Gerco Dries <gerco.dries@adaptris.com>
2016-06-30 20:40:14 -0500

    Remove support for JRE5

--- messagemanager-ui/src/main/java/nl/queuemanager/ui/PreconnectUIModule.java
+++ messagemanager-ui/src/main/java/nl/queuemanager/ui/PreconnectUIModule.java
@@ -1,11 +1,10 @@
 package nl.queuemanager.ui;
 
-import nl.queuemanager.ui.util.DesktopHelper;
-import nl.queuemanager.ui.util.DesktopHelperJRE5;
-import nl.queuemanager.ui.util.DesktopHelperJRE6;
-
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
+
+import nl.queuemanager.ui.util.DesktopHelper;
+import nl.queuemanager.ui.util.DesktopHelperJRE6;
 
 public class PreconnectUIModule extends AbstractModule {
 
@@ -14,13 +13,7 @@ public class PreconnectUIModule extends AbstractModule {
 		// To display errors to the user, we need a global error listener for the task executor
 		bind(TaskErrorListener.class).in(Scopes.SINGLETON);
 				
-		try {
-			Class.forName("java.awt.Desktop");
-			bind(DesktopHelper.class).to(DesktopHelperJRE6.class).in(Scopes.SINGLETON);
-		} catch (ClassNotFoundException e) {
-			bind(DesktopHelper.class).to(DesktopHelperJRE5.class).in(Scopes.SINGLETON);
-		}
-		
+		bind(DesktopHelper.class).to(DesktopHelperJRE6.class).in(Scopes.SINGLETON);
 	}
 	
 }
