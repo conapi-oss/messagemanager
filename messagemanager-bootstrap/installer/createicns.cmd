5559786d4279da1b59b3bef36b1e270ee7bdc3c3
Stefan Fritz <stefan@conapi.at>
2024-03-27 13:48:52 +0100

    - msi installer

---
+++ messagemanager-bootstrap/installer/createicns.cmd
@@ -0,0 +1,32 @@
+rem using https://github.com/relikd/icnsutil
+rem pip3 install icnsutil
+rem pip install Pillow
+rem https://relikd.github.io/icnsutil/html/viewer.html
+
+rem convert to rgb first 
+icnsutil img  ../src/main/resources/icons/conapi_16x16.argb ../src/main/resources/icons/conapi_16x16.png
+icnsutil img  ../src/main/resources/icons/conapi_32x32.argb ../src/main/resources/icons/conapi_32x32.png
+icnsutil img  ../src/main/resources/icons/conapi_48x48.argb ../src/main/resources/icons/conapi_48x48.png
+icnsutil img  ../src/main/resources/icons/conapi_64x64.argb ../src/main/resources/icons/conapi_64x64.png
+icnsutil img  ../src/main/resources/icons/conapi_128x128.argb ../src/main/resources/icons/conapi_128x128.png
+
+icnsutil img  ../src/main/resources/icons/conapi_16x16.rgb ../src/main/resources/icons/conapi_16x16.png
+icnsutil img  ../src/main/resources/icons/conapi_32x32.rgb ../src/main/resources/icons/conapi_32x32.png
+icnsutil img  ../src/main/resources/icons/conapi_48x48.rgb ../src/main/resources/icons/conapi_48x48.png
+icnsutil img  ../src/main/resources/icons/conapi_64x64.rgb ../src/main/resources/icons/conapi_64x64.png
+icnsutil img  ../src/main/resources/icons/conapi_128x128.rgb ../src/main/resources/icons/conapi_128x128.png
+
+
+rem icnsutil c -f ../src/main/resources/icons/conapi.icns ../src/main/resources/icons/conapi_16x16.argb ../src/main/resources/icons/conapi_32x32.argb ../src/main/resources/icons/conapi_16x16.rgb ../src/main/resources/icons/conapi_32x32.rgb ../src/main/resources/icons/conapi_48x48.rgb ../src/main/resources/icons/conapi_64x64.rgb ../src/main/resources/icons/conapi_128x128.rgb ../src/main/resources/icons/conapi_16x16.rgb.mask ../src/main/resources/icons/conapi_32x32.rgb.mask ../src/main/resources/icons/conapi_48x48.rgb.mask ../src/main/resources/icons/conapi_64x64.rgb.mask ../src/main/resources/icons/conapi_128x128.rgb.mask
+
+icnsutil c -f ../src/main/resources/icons/conapi.icns  ../src/main/resources/icons/conapi_16x16.rgb ../src/main/resources/icons/conapi_32x32.rgb ../src/main/resources/icons/conapi_16x16.rgb.mask ../src/main/resources/icons/conapi_32x32.rgb.mask ../src/main/resources/icons/conapi_48x48.rgb ../src/main/resources/icons/conapi_48x48.rgb.mask ../src/main/resources/icons/conapi_128x128.rgb ../src/main/resources/icons/conapi_128x128.rgb.mask
+rem ../src/main/resources/icons/conapi_48x48.rgb ../src/main/resources/icons/conapi_64x64.rgb ../src/main/resources/icons/conapi_128x128.rgb  ../src/main/resources/icons/conapi_48x48.rgb.mask ../src/main/resources/icons/conapi_64x64.rgb.mask ../src/main/resources/icons/conapi_128x128.rgb.mask
+
+ rem not sure why but below do not work
+ rem ../src/main/resources/icons/conapi_48x48.argb
+ rem ../src/main/resources/icons/conapi_64x64.argb
+ rem ../src/main/resources/icons/conapi_128x128.argb
+icnsutil i ../src/main/resources/icons/conapi.icns
+
+
+
--- messagemanager-bootstrap/scripts/launch.cmd
+++ messagemanager-bootstrap/scripts/launch.cmd
@@ -1,16 +1,26 @@
-@echo off
+@echo off&setlocal
+
+rem get the parent folder
+for %%i in ("%~dp0..") do set "INSTALL_DIR=%%~fi"
 
 rem set via setenv if needed
 set UPDATE_URL="https://product.conapi.at/messagemanager/setup.xml"
 
 set JVM_OPTS=-Djavafx.embed.singleThread=true
 
-if "%JAVA_HOME%"=="" (
-   set LAUNCHER_JAVA=java
+rem use shiped JRE if available, otherwise fallback to JAVA_HOME and PATH
+if exist "%INSTALL_DIR%\jre\" (
+    set LAUNCHER_JAVA=%INSTALL_DIR%\jre\bin\javaw.exe
+    set LAUNCHER_JAVA_DEBUG=%INSTALL_DIR%\jre\bin\java.exe
 ) else (
-   set LAUNCHER_JAVA=%JAVA_HOME%\bin\java.exe
+    if "%JAVA_HOME%"=="" (
+       set LAUNCHER_JAVA=javaw
+       set LAUNCHER_JAVA_DEBUG=java
+    ) else (
+       set LAUNCHER_JAVA=%JAVA_HOME%\bin\java.exe
+       set LAUNCHER_JAVA_DEBUG=%JAVA_HOME%\bin\java.exe
+    )
 )
-
 
 rem  assuming this is run from bin
 cd ..
@@ -19,7 +29,7 @@ rem allow user to set additional JVM options i.e. truststore
 if exist ".\bin\setenv.cmd" call ".\bin\setenv.cmd"
 
 if "%~1"=="debug" (
-    call %LAUNCHER_JAVA% %JVM_OPTS% -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -p bootstrap\update4j.jar --add-modules jdk.unsupported,java.scripting -m org.update4j/org.update4j.Bootstrap --remote %UPDATE_URL% --syncLocal --local bootstrap.xml --launchFirst
+    call %LAUNCHER_JAVA_DEBUG% %JVM_OPTS% -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -p bootstrap\update4j.jar --add-modules jdk.unsupported,java.scripting -m org.update4j/org.update4j.Bootstrap --remote %UPDATE_URL% --syncLocal --local bootstrap.xml --launchFirst
 ) else (
     start %LAUNCHER_JAVA% %JVM_OPTS% -p bootstrap\update4j.jar --add-modules jdk.unsupported,java.scripting -m org.update4j/org.update4j.Bootstrap --remote %UPDATE_URL% --syncLocal --local bootstrap.xml --launchFirst
 )
