62bab3f3749a6043dac8201bd7d93b2463fda57e
Stefan Fritz <stefan@conapi.at>
2024-05-17 17:11:45 +0200

    bump lombok version

--- messagemanager-sonicmq/src/main/java/nl/queuemanager/smm/Version.java
+++ messagemanager-sonicmq/src/main/java/nl/queuemanager/smm/Version.java
@@ -8,6 +8,6 @@ public class Version {
 
 	public static final String VERSION = "4.0-SNAPSHOT";
 	//public static final String SUFFIX = "${suffix}";
-	public static final String BUILD_TIMESTAMP = "Fri May 17 16:18:30 CEST 2024";
+	public static final String BUILD_TIMESTAMP = "Fri May 17 16:39:30 CEST 2024";
 	public static final String BUILD_ID = "4.0-SNAPSHOT";
 }
