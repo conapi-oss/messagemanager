d6b29af0e7c4d193989d29ddd45b4458ac6d2e8d
Stefan Fritz <stefan@conapi.at>
2024-07-22 13:59:32 +0200

    Increment version to 4.0.1

--- messagemanager-app/src/main/java/nl/queuemanager/app/Version.java
+++ messagemanager-app/src/main/java/nl/queuemanager/app/Version.java
@@ -6,8 +6,8 @@ public class Version {
 		return VERSION;
 	}
 
-	public static final String VERSION = "4.0";
+	public static final String VERSION = "4.0.1";
 	//public static final String SUFFIX = "${suffix}";
-	public static final String BUILD_TIMESTAMP = "Fri Jul 19 15:22:20 CEST 2024";
-	public static final String BUILD_ID = "4.0";
+	public static final String BUILD_TIMESTAMP = "Mon Jul 22 13:59:24 CEST 2024";
+	public static final String BUILD_ID = "4.0.1";
 }
