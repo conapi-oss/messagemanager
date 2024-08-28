0505d17fe719e9d13e7ed7c17b46b7766962c5d4
Stefan Fritz <stefan@conapi.at>
2024-08-28 10:55:05 +0200

    move to 4.0.7

--- messagemanager-core/src/main/java/nl/queuemanager/core/Version.java
+++ messagemanager-core/src/main/java/nl/queuemanager/core/Version.java
@@ -6,8 +6,8 @@ public class Version {
 		return VERSION;
 	}
 
-	public static final String VERSION = "4.0.5";
+	public static final String VERSION = "4.0.6";
 	//public static final String SUFFIX = "${suffix}";
-	public static final String BUILD_TIMESTAMP = "Wed Aug 28 09:43:41 CEST 2024";
-	public static final String BUILD_ID = "4.0.5";
+	public static final String BUILD_TIMESTAMP = "Wed Aug 28 10:33:28 CEST 2024";
+	public static final String BUILD_ID = "4.0.6";
 }
