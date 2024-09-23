1a14544dddf77c2d77aa1dd75205d1fc65a473b0
Stefan Fritz <stefan@conapi.at>
2024-09-23 15:55:29 +0200

    - video for scripting

--- messagemanager-app/src/main/java/nl/queuemanager/app/Version.java
+++ messagemanager-app/src/main/java/nl/queuemanager/app/Version.java
@@ -8,6 +8,6 @@ public class Version {
 
 	public static final String VERSION = "4.1.0";
 	//public static final String SUFFIX = "${suffix}";
-	public static final String BUILD_TIMESTAMP = "Thu Sep 19 11:35:16 CEST 2024";
+	public static final String BUILD_TIMESTAMP = "Mon Sep 23 14:59:25 CEST 2024";
 	public static final String BUILD_ID = "4.1.0";
 }
