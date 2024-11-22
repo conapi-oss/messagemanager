449d8e770d7fa384f19b98d62e07f30bde4153e0
Stefan Fritz <90251049+conapi-stefan@users.noreply.github.com>
2024-11-22 08:59:07 +0100

    - DMG build for ARM and x64 (no codesign)

--- messagemanager-app/src/main/java/nl/queuemanager/app/Version.java
+++ messagemanager-app/src/main/java/nl/queuemanager/app/Version.java
@@ -8,6 +8,6 @@ public class Version {
 
 	public static final String VERSION = "4.1.1";
 	//public static final String SUFFIX = "${suffix}";
-	public static final String BUILD_TIMESTAMP = "Wed Nov 20 11:46:39 CET 2024";
+	public static final String BUILD_TIMESTAMP = "Fri Nov 22 08:44:00 CET 2024";
 	public static final String BUILD_ID = "4.1.1";
 }
