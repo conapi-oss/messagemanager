e245975b98d53eb27aa73cb73ab83017a9cfc231
Stefan Fritz <90251049+conapi-stefan@users.noreply.github.com>
2024-12-05 11:05:17 +0100

    branding updates

--- messagemanager-core/src/main/java/nl/queuemanager/core/Version.java
+++ messagemanager-core/src/main/java/nl/queuemanager/core/Version.java
@@ -8,6 +8,6 @@ public class Version {
 
 	public static final String VERSION = "4.1.1";
 	//public static final String SUFFIX = "${suffix}";
-	public static final String BUILD_TIMESTAMP = "Mon Dec 02 11:13:01 CET 2024";
+	public static final String BUILD_TIMESTAMP = "Thu Dec 05 11:00:41 CET 2024";
 	public static final String BUILD_ID = "4.1.1";
 }
